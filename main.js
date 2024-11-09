cd42adf78d7bde947d5137caceed5487217552fb
Paulo Vieira <paulo_vieira22@hotmail.com>
2024-11-09 06:59:23 -0300

    project final

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ $(document).ready(function() {
     $('button').click(function(e) {
         e.preventDefault();
 
-        const task = $('#tarefa').val(); // Pega o valor da tarefa
-        if (task) { // Verifica se o campo de tarefa não está vazio
-            const tarefa = $('<li></li>').text(task); // Cria um novo item de lista com o texto da tarefa
+        const task = $('#tarefa').val(); 
+        if (task) { 
+            const tarefa = $('<li></li>').text(task);
 
             const tarefaCount = $('#lista-tarefas li').length;
             if (tarefaCount % 2 === 0) {
-                tarefa.css('background-color', '#ecc656'); // Exemplo de cor clara para o fundo
+                tarefa.css('background-color', '#ecc656');
             }else{
-                tarefa.css('background-color', '#f8efcf'); // Exemplo de cor clara para o fundo
+                tarefa.css('background-color', '#f8efcf');
 
             }
 
-            $('#lista-tarefas').append(tarefa); // Adiciona o item à lista de tarefas
-            $('#tarefa').val(''); // Limpa o campo de entrada
+            $('#lista-tarefas').append(tarefa);
+            $('#tarefa').val(''); 
 
             tarefa.on('click', function() {
                 $(this).toggleClass('line-through');
